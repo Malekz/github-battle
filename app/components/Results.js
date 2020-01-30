e7e1ea2ed167c536f1fda1d8666d72cfe2fdc921
malekz <malek.zarzour@gmail.com>
2020-01-30 23:28:32 +0100

    deleted playerOne & playerTwo proptypes

--- app/components/Results.js
+++ app/components/Results.js
@@ -12,6 +12,9 @@ import Card from '../components/Card'
 import PropTypes from 'prop-types'
 import Loading from './Loading'
 import Tooltip from './Tooltip'
+import queryString from 'query-string'
+import { Link } from 'react-router-dom'
+
 
 
 function ProfileList({ profile }) {
@@ -61,7 +64,7 @@ export default class Results extends React.Component {
     };
   }
   componentDidMount() {
-    const { playerOne, playerTwo } = this.props;
+    const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
 
     battle([playerOne, playerTwo])
       .then(players => {
@@ -102,9 +105,9 @@ export default class Results extends React.Component {
             <ProfileList profile={loser.profile} />
           </Card>
         </div>
-        <button onClick={this.props.onReset} className='btn dark-btn btn-space'>
+        <Link to='/battle' className='btn dark-btn btn-space'>
           Reset
-        </button>
+        </Link>
       </React.Fragment>
     );
   }
@@ -112,10 +115,4 @@ export default class Results extends React.Component {
 
 ProfileList.propTypes = {
   profile: PropTypes.object.isRequired
-}
-
-Results.propTypes = {
-  playerOne: PropTypes.string.isRequired,
-  playerTwo: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
 }
