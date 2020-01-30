1a3b15cfb9c486f08f68e058266c313f9e2a63ee
malekz <malek.zarzour@gmail.com>
2020-01-30 16:47:38 +0100

    installed query-string package

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -8,6 +8,7 @@ import {
 import PropTypes from "prop-types";
 import Results from "./Results";
 import { ThemeConsumer } from '../contexts/theme'
+import { Link } from 'react-router-dom'
 
 function Instructions() {
   return (
@@ -137,8 +138,7 @@ export default class Battle extends React.Component {
 
     this.state = {
       playerOne: null,
-      playerTwo: null,
-      battle: false
+      playerTwo: null
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -155,13 +155,9 @@ export default class Battle extends React.Component {
     });
   }
   render() {
-    const { playerOne, playerTwo, battle } = this.state;
+    const { playerOne, playerTwo } = this.state;
 
-    if (battle === true) {
-      return <Results playerOne={playerOne} playerTwo={playerTwo} onReset={() => this.setState({
-        playerOne: null, playerTwo: null, battle: false
-      })} />;
-    }
+
 
     return (
       <React.Fragment>
@@ -198,12 +194,14 @@ export default class Battle extends React.Component {
           </div>
 
           {playerOne && playerTwo && (
-            <button
+            <Link
               className="btn dark-btn btn-space"
-              onClick={() => this.setState({ battle: true })}
+              to={{
+                pathname: '/battle/results', search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+              }}
             >
               Battle
-            </button>
+            </Link>
           )}
         </div>
       </React.Fragment>
